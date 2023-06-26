bcc982a95ba5e66ab8326f69433e974c5e87f80d
nicolas delille <delille.n@gmail.com>
2023-06-26 15:21:47 +0200

    console.log removed

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("HOP");
-
 const titleArray = [
                     "Salut !",
                     "Comment ça va ?",
@@ -11,11 +9,8 @@ const titleArray = [
 
 const greetingTitle = arr => {
     const randomNumber = Math.floor(Math.random() * 6);
-    console.log(`Random Number is: ${randomNumber}`);
     let title = document.getElementById("myTitle");
-    console.log(`Document by id: ${title}`);
     title.innerHTML = arr[randomNumber];
 };
 
 greetingTitle(titleArray);
-console.log(titleArray);
